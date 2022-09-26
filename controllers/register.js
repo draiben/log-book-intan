e72e14df1faf57b6fc45bd66d2a94dc994697b91
draiben <enggal.draiben@gmail.com>
2022-09-26 10:18:33 +0700

    create register

---
+++ controllers/register.js
@@ -0,0 +1,40 @@
+const db = require("../routes/db-config");
+const bcrypt = require("bcryptjs");
+
+const register = async (req, res) => {
+    const { nama, email, password } = req.body;
+
+    if (!nama || !email || !password) return res.json({ status: "error", error: "Silahkan masukkan email dan password anda!" });
+    else {
+        db.query("SELECT email FROM users WHERE email = ?", [email], async (err, results) => {
+            if (err) throw err;
+            if (results[0]) return res.json({ status: "error", error: "Email telah terdaftar" });
+            else {
+                // Menambahkan ID autoincrement
+                db.query("SELECT max(`id_user`) as maxId FROM `users`", async (err, result) => {
+                    // Get year
+                    const getYear = new Date().getFullYear();
+                    var newId = "";
+
+                    // Cek ID
+                    if (!result[0].maxId) {
+                        newId += String("A" + getYear + 1);
+                    } else {
+                        var oldId = result[0].maxId;
+                        var incrementId = oldId.substring(5);
+                        var firstId = "A";
+                        newId += String(firstId + getYear + ++incrementId);
+                    }
+                    console.log(newId);
+                    const Npassword = await bcrypt.hash(password, 8);
+                    db.query("INSERT INTO users SET ?", { id_user: newId, nama: nama, email: email, password: Npassword }, async (error, result) => {
+                        if (error) throw error;
+                        return res.json({ status: "success", success: "Email berhasil didaftarkan" });
+                    });
+                });
+            }
+        });
+    }
+};
+
+module.exports = register;
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const db = require("./routes/db-config");
+const cookie = require("cookie-parser");
+
+const PORT = process.env.PORT || 5000;
+
+// Setup for view
+app.use("/js", express.static(__dirname + "/public/js"));
+app.use("/css", express.static(__dirname + "/public/css"));
+app.use("/assets", express.static(__dirname + "/public/assets"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// Use cookie and json
+app.use(cookie());
+app.use(express.json());
+
+// Connect to database
+db.connect((err) => {
+    if (err) throw err;
+    console.log("Database connected");
+});
+
+// Routing
+app.use("/", require("./routes/pages"));
+app.use("/api", require("./controllers/auth"));
+
+// Run server
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
